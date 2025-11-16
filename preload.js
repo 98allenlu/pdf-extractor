26fb1a2da5ba35c2b06cd111e712fc5a8ccfd0e4
98allenlu <85421725+98allenlu@users.noreply.github.com>
2025-11-16 16:14:49 -0500

    Expose Electron API methods in preload.js

--- preload.js
+++ preload.js
@@ -1,1 +1,16 @@
+const { contextBridge, ipcRenderer } = require('electron');
 
+contextBridge.exposeInMainWorld('electronAPI', {
+    // File selection dialogs
+    openFileDialog: () => ipcRenderer.invoke('open-file-dialog'),
+    selectSaveDirectory: () => ipcRenderer.invoke('select-save-directory'),
+    
+    // Core processing function
+    startProcessing: (payload) => ipcRenderer.invoke('start-processing', payload),
+    
+    // Single download for convenience
+    downloadSingle: (item) => ipcRenderer.invoke('download-single', item),
+
+    // Listener for status updates from the main process
+    onUpdateStatus: (callback) => ipcRenderer.on('update-status', (event, message) => callback(message))
+});
